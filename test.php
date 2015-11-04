969753e3040527cbcf30646b129a3c66297aa469
root <root@jeremy.com>
2015-11-04 12:08:09 +0100

    Cours du 02/11/2015

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+$animal = "chat";
+if($animal == "chien" || $animal == "chat"){
+   echo("Bienvenue cher animal a quatre pattes");
+   return true;
+}
+echo("Il n'y a que les chiens ou les chats qui sont autorisés !");
+?> 
